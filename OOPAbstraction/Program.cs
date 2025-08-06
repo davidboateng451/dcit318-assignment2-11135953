2289393a880b32a35cee2f1d78ea625a8a24568d
davidboateng451 <151746606+davidboateng451@users.noreply.github.com>
2025-08-06 09:35:32 +0000

    files for OOPAbstraction

---
+++ OOPAbstraction/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace OOPAbstraction
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override double GetArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5.0);
+            Shape rectangle = new Rectangle(4.0, 6.0);
+
+            Console.WriteLine($"Circle Area: {circle.GetArea():F2}");
+            Console.WriteLine($"Rectangle Area: {rectangle.GetArea():F2}");
+
+            Console.ReadLine();
+        }
+    }
+}
